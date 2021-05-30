4923af64558fc4ca498ddc924fb0a57e019c330b
bivas-dopehead <83751814+bivas-dopehead@users.noreply.github.com>
2021-05-31 00:33:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -173,8 +173,4 @@ function App() {
   }
  </div>);
 }
-
-function ShowTown({towns}){ return <div>         {             towns.map(                 (town ,index )=><li id={"town"+(1+index)}>{town.name}</li>             )         }     </div>}
-function ShowCity({city}){  const[a,setA]=useState(0)  return <div>              {                city.map(                  (c,index)=><div>                             <li id={"city"+(index+1)} onClick={                                 ()=>{                                     if(a===c.name)                                       setA(0)                                      else                                      setA(c.name)                                 }                             }>{c.name}</li>                             {a===c.name && <ShowTown towns={c.towns}/>}                      </div>                )              }         </div>;
-}
 export default App;
